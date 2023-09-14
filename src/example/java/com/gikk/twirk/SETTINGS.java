dd0f00d697c4d2cb689853aa79597352090957b8
Pkrock <R_c_rzew28@hotmail.com>
2023-09-13 23:18:02 -0500

    Update SETTINGS.java

--- src/example/java/com/gikk/twirk/SETTINGS.java
+++ src/example/java/com/gikk/twirk/SETTINGS.java
@@ -1,6 +1,6 @@
 package com.gikk.twirk;
 
 public class SETTINGS {
-	public static final String MY_PASS = "oauth:x8k5k9xggbvh3lfm1ax5umm7csmic9";
-	public static final String MY_NICK = "pkrock";
+	public static final String MY_PASS = "oauth:r3rd9fvgxxeqln263qp5xssvr0olbb";
+	public static final String MY_NICK = "PKRockBot";
 }
