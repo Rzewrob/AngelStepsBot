8f6b314b558fb7f0a818f2ae4a686ac72d380028
Crabgram <94872307+Crabgram@users.noreply.github.com>
2023-10-06 23:12:47 -0400

    SETTINGS.java

--- src/example/java/com/gikk/twirk/SETTINGS.java
+++ src/example/java/com/gikk/twirk/SETTINGS.java
@@ -1,6 +1,6 @@
 package com.gikk.twirk;
 
 public class SETTINGS {
-	public static final String MY_PASS = "oauth:r3rd9fvgxxeqln263qp5xssvr0olbb";
-	public static final String MY_NICK = "PKRockBot";
+	public static final String MY_PASS = "oauth:";
+	public static final String MY_NICK = "";
 }
